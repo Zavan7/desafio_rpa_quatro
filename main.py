a5c21a60c750198f063cc95fa5ae9bc8d8ae9ad3
Vitor Zavan <vitorzavanj@gmail.com>
2026-04-21 02:37:18 -0300

    primeiros passos

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def main():
-    print("Hello from rpa-uv-quatro!")
 
 
-if __name__ == "__main__":
-    main()
+
+url = 'https://practicetestautomation.com/'
---
+++ pages/initial_page.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page
+import logging
+
+logger = logging.getLogger(__name__)
+
+class InitialPage:
+    def __init__(self, page: Page, url: str, timeout=4000):
+        self.page = page
+        self.url = url
+        self.timeout = timeout
+
+    def home_page(self):
+        try:
+            self.page.goto(self.url)
+
+        except Exception as e:
+            return
