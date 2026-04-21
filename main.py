df45d85390c6061bd2027a174bbc4f314801b14c
Vitor Zavan <vitorzavanj@gmail.com>
2026-04-21 02:50:45 -0300

    primeiros passos

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+from pages.initial_page import InitialPage
+from playwright.sync_api import sync_playwright
 
+import time
 
 
 url = 'https://practicetestautomation.com/'
+
+with sync_playwright() as p:
+    browser = p.firefox.launch(headless=False)
+    page = browser.new_page()
+
+    initial_page = InitialPage(page, url)
+    initial_page.home_page()
+
+    time.sleep(5)
+    browser.close()
